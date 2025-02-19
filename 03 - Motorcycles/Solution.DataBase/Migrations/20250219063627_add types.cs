d431e3a2a99033a3062978b2efff3a8babfe6169
turosgolyo <papp.dani2206@gmail.com>
2025-02-19 07:42:21 +0100

    dolgozat 2. feladat

---
+++ 03 - Motorcycles/Solution.DataBase/Migrations/20250219063627_add types.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Solution.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var query = @$"
+                insert into
+                    [Type]
+                    ([Name])
+                values
+                    ('Crusier'),
+                    ('Classic'),
+                    ('Veteran'),
+                    ('Cross'),
+                    ('Pitpike'),
+                    ('Enduro'),
+                    ('Kids Bike'),
+                    ('Sport'),
+                    ('Quad'),
+                    ('ATV'),
+                    ('RUV'),
+                    ('SSV'),
+                    ('UTV'),
+                    ('Scooter'),
+                    ('Moped'),
+                    ('Supermoto'),
+                    ('Trial'),
+                    ('Trike'),
+                    ('Tour'),
+                    ('Naked'),
+                    ('Sport Tour')
+            ";
+
+            migrationBuilder.Sql(query);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var query = $"truncate table [Type]";
+
+            migrationBuilder.Sql(query);
+        }
+    }
+}
